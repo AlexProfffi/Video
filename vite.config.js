9b1ce52f0690e62fcb0d443ec5c131377e5961bb
AlexProfffi <prakticanrad2017@gmail.com>
2024-07-21 17:38:52 +0300

    start

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
+
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: [
+                'app/**',
+                'resources/views/**',
+                'routes/**',
+                'config/**'
+            ],
+        })
+    ],
+    server: {
+        host: 'localhost',
+        port: 3000
+    },
+    resolve: {
+        alias: {
+            '@': '/resources/js',
+            'ziggy': path.resolve('vendor/tightenco/ziggy')
+        }
+    }
+});
